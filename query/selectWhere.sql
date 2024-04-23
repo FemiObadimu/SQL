c05cc8b35ebc1acac4c797b877c34194b883c8ff
Femi Obadimu <ficazzosam@gmail.com>
2024-04-23 14:23:53 +0100

    added select where

--- query/selectWhere.sql
+++ query/selectWhere.sql
@@ -39,3 +39,17 @@ FROM film
 WHERE rating = 'R'
     OR rating = 'PG-13';
 -- this returns only the COUNT value of the column where the rental rate, replacement cost and rating are truthy based off thr conditions
+-- 
+SELECT email
+FROM customer
+WHERE first_name = 'Nancy'
+    AND last_name = 'Thomas';
+-- returns only the email of Nancy Thomas
+-- 
+SELECT description
+FROM film
+WHERE title = 'Outlaw Hanky' -- returns the description of the movie [Outlaw Hanky]
+    -- 
+SELECT phone
+FROM address
+WHERE address = '259 Ipoh Drive' --  returns the phone number of the customer that stays in 259, Ipoh Drive
