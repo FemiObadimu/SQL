86264b312069a1d0ce7cbbe39a22ef5bcd13582c
Femi Obadimu <ficazzosam@gmail.com>
2024-04-23 12:49:07 +0100

    first direct push of sql queries

---
+++ query/select.sql
@@ -0,0 +1,6 @@
+SELECT *
+FROM film -- This selects all the columms on the films table
+SELECT first_name,
+    last_name,
+    email
+FROM customer --  This finds and returns all the columns of firstname, lastname, emails respectively from the customer table
