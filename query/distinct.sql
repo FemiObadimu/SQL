4600d047d26a022ef0a974ca14d7fdd94ccc1c74
Femi Obadimu <ficazzosam@gmail.com>
2024-04-23 13:10:53 +0100

    added select distinct

--- query/distinct.sql
+++ query/distinct.sql
@@ -1,1 +1,5 @@
 -- DISTINCT SELECT
+-- This is used to get distinct values only, for example if we have 500+` ratings on different movies, but the ratings are only [PG-13, PG-10, PG-16, PG-30, NG-Adult]
+SELECT DISTINCT rating
+FROM film;
+--  This returns only ratings that are availble without repetition
