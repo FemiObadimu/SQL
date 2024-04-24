abbe624eb414b4648ad6c8618c26e00610ac0a02
Femi Obadimu <ficazzosam@gmail.com>
2024-04-24 10:56:41 +0100

    added in and not in query commands

--- query/in.sql
+++ query/in.sql
@@ -2,3 +2,6 @@ SELECT COUNT(*)
 FROM payment
 WHERE amount IN (0.99, 1.98, 1.99);
 -- returns amount that fall in these range of values
+SELECT *
+FROM customer
+WHERE first_name IN ('John', 'Jake', 'Julie') --  returns the  whole row (if the names in the parameter exists)
--- query/notin.sql
+++ query/notin.sql
@@ -2,3 +2,6 @@ SELECT COUNT(*)
 FROM payment
 WHERE amount NOT IN (0.99, 1.98, 1.99);
 -- return amounts that are not un these range of values
+SELECT *
+FROM customer
+WHERE first_name NOT IN ('John', 'Jake', 'Julie') --  returns the  whole row of table data without the first names of Johm, Julie, Jake
