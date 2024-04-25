4e307c77029262cfcd8bc43886cf623f19ccaf9d
Femi Obadimu <ficazzosam@gmail.com>
2024-04-25 10:17:20 +0100

    added having

--- aggregates/having.sql
+++ aggregates/having.sql
@@ -5,3 +5,20 @@ FROM payment
 GROUP BY customer_id
 HAVING SUM(amount) > 100;
 -- returns the customerIds with sum of amounts greater than 100
+--
+
+SELECT store_id,
+    COUNT(customer_id)
+FROM customer
+GROUP BY store_id
+HAVING COUNT(customer_id) > 300;
+-- retutns the id's of the store and the total number of customers thats visited
+--
+
+SELECT customer_id,
+    COUNT(amount)
+FROM payment
+GROUP BY customer_id
+HAVING COUNT(amount) >= 40;
+-- returns the ids of customers with transactions greater than 40  
+--
